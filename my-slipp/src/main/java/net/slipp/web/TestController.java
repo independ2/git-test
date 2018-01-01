8d03eac76074efbad0b6a3db0350b6d935494b37
Hero57\okcom15 <independ2@naver.com>
2018-01-01 13:32:12 +0900

    2.0

--- my-slipp/src/main/java/net/slipp/web/TestController.java
+++ my-slipp/src/main/java/net/slipp/web/TestController.java
@@ -1,13 +1,18 @@
-package net.slipp;
+package net.slipp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 	@GetMapping("/")
-	public String Index(){
+	public String Index(Model model){
 		return "index";
-		
 	}
+	@GetMapping("/form")
+	public String Form(){
+		return "form";
+	}
+
 }
